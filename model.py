2760dfdca7c47af26f6d0a1b8e23cff6ba5e7979
nonameuser2019 <zhe.depotop@gmail.com>
2020-01-22 22:59:57 +0200

    Add func photo and other func

--- model.py
+++ model.py
@@ -17,14 +17,16 @@ class Armani(Base):
     discount_price = Column(Float)
     product_id = Column(String)
     size = Column(String)
+    img_name = Column(String)
 
-    def __init__(self, product_name, color, full_price, discount_price, product_id, size):
+    def __init__(self, product_name, color, full_price, discount_price, product_id, size, img_name):
         self.product_name = product_name
         self.color = color
         self.full_price = full_price
         self.discount_price = discount_price
         self.product_id = product_id
         self.size = size
+        self.img_name = img_name
 
     def __repr__(self):
         return "CData '%s'" % (self.url)
--- parser.py
+++ parser.py
@@ -9,13 +9,51 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, Text, String, Float
 from model import *
+import os
 
 
+count_photo = 0
 proxy = {'HTTPS': '163.172.182.164:3128'}
 ua = UserAgent()
 HEADERS = {
     'user-agent': ua.random
 }
+
+
+def create_dir_name():
+    dir_name = 'images'
+    try:
+        os.mkdir(dir_name)
+    except OSError:
+        print('Папка существует')
+    return dir_name
+
+
+def get_photo(name_photo, dir_name):
+    image_list = []
+    img_name = []
+    image_list.append(name_photo + '_13_a.jpg')
+    image_list.append(name_photo + '_13_b.jpg')
+    image_list.append(name_photo + '_13_r.jpg')
+    image_list.append(name_photo + '_13_d.jpg')
+    image_list.append(name_photo + '_13_f.jpg')
+    image_list.append(name_photo + '_13_e.jpg')
+    print(image_list)
+    for img in image_list:
+        try:
+            global count_photo
+            photo_name = count_photo
+            file_obj = requests.get(img, stream=True)
+            if file_obj.status_code == 200:
+                print(file_obj.status_code)
+                with open(dir_name+'/'+str(photo_name)+'.jpg', 'bw') as photo:
+                    for chunk in file_obj.iter_content(8192):
+                        photo.write(chunk)
+                count_photo +=1
+                img_name.append(str(photo_name))
+        except Exception as e:
+            print(e)
+    return img_name
 
 
 def get_html(url, payload=None):
@@ -45,7 +83,9 @@ def get_page_count(html):
         print('Error page count not defined')
 
 
-def parser_card(html):
+
+
+def parser_list(html):
     soup = BeautifulSoup(html.content, 'html.parser')
     try:
         section = soup.find('main', id='main').find_all('article', class_='item')
@@ -74,22 +114,34 @@ def parser_card(html):
         except:
             product_id = None
 
+        card_url = item.find('a')['href']
+        try:
+            img = item.find('img', class_='mainImage')['data-origin']
+            img_main_photo = img[:img.find('_')]
+
+        except:
+            img = item.find('img', class_='mainImage')['src']
+            img_main_photo = img[:img.find('_')]
+
+        # вызов функции, которая скачивает все фото и возвращает лист с их именами
+        img_name_list = get_photo(img_main_photo, create_dir_name())
         # временная переменная, заменить потом размером
         size = None
 
         Session = sessionmaker(bind=db_engine)
         session = Session()
-        new_element = Armani(product_name, color, full_price, discount_price, product_id, size)
+        new_element = Armani(product_name, color, full_price, discount_price, product_id, size, ','.join(img_name_list))
         session.add(new_element)
         session.commit()
 
 
 
 def main():
-    html = get_html('https://www.armaniexchange.com/us/men/graphictees?page=1')
+    cat_url = 'https://www.armaniexchange.com/us/men/graphictees'
+    html = get_html(cat_url)
     page_count = get_page_count(html)
-    print(page_count)
-    parser_card(html)
+    for count in range(1, page_count +1 ):
+        parser_list(get_html(cat_url + '?page=' + str(count)))
 
 
 if __name__ == '__main__':
